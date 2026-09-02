8f69263cfb35e14154cc41dfaac4a068923d11d1
fortune-c <fortunate.adesinas@gmail.com>
2026-09-02 23:08:25 +0100

    fix cache folder issues

--- today.py
+++ today.py
@@ -225,6 +225,7 @@ def cache_builder(edges, comment_size, force_cache, loc_add=0, loc_del=0):
     """
     cached = True # Assume all repositories are cached
     filename = 'cache/'+hashlib.sha256(USER_NAME.encode('utf-8')).hexdigest()+'.txt' # Create a unique filename for each user
+    os.makedirs('cache', exist_ok=True)
     try:
         with open(filename, 'r') as f:
             data = f.readlines()
